031878779057c05a5297e0535fbbc0605f464bd9
Braeae <91569703+Braeae@users.noreply.github.com>
2022-02-01 22:17:55 +0100

    Links added

--- src/Weather.js
+++ src/Weather.js
@@ -57,6 +57,26 @@ function Weather(props) {
 
           <WeatherData data={weatherInfo} />
         </div>
+        <p>
+          Open-sourced on
+          <a
+            href="https://github.com/Braeae/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            GitHub{" "}
+          </a>
+          and hosted on
+          <a
+            href="https://suspicious-easley-0813d1.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            Netlify
+          </a>
+        </p>
       </div>
     );
   } else {
