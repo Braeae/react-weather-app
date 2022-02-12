d20c3bc190e08b2b17cd05cc2c02d6c93e4f8dcd
Braeae <91569703+Braeae@users.noreply.github.com>
2022-02-12 14:08:40 +0100

    CSS changes

--- src/App.js
+++ src/App.js
@@ -8,22 +8,20 @@ function App() {
         <Weather defaultCity="Amsterdam" />
       </div>
       <p className="AppInfo m-2">
-        Open-sourced on
+        Open-sourced on{" "}
         <a
           href="https://github.com/Braeae/react-weather-app"
           target="_blank"
           rel="noreferrer"
         >
-          {" "}
-          GitHub{" "}
-        </a>
-        and hosted on
+          GitHub
+        </a>{" "}
+        and hosted on{" "}
         <a
           href="https://suspicious-easley-0813d1.netlify.app/"
           target="_blank"
           rel="noreferrer"
         >
-          {" "}
           Netlify
         </a>
       </p>
